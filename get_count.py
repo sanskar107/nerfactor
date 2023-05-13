952bcb57c21aab74f470713edbf6b9de2953501a
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2023-05-13 00:38:39 -0700

    update get_count

--- get_count.py
+++ get_count.py
@@ -4,7 +4,7 @@ from glob import glob
 
 assert len(sys.argv)==2, "Pass scene name"
 
-files = os.listdir('/export/work/sanskar/nerfactor_without_neus_data/input/' + sys.argv[1])
+files = os.listdir('/export/work/sanskar/nerfactor/input/' + sys.argv[1])
 files = [x for x in files if x.startswith('train_')]
 a = len(files)
 
